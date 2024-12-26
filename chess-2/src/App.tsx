6bfad1550e40ca10e84ca207c8aa9e600d8a3a3b
= <robillard123@gmail.com>
2024-12-25 20:51:41 -0500

    working chess interface

--- chess-2/src/App.tsx
+++ chess-2/src/App.tsx
@@ -1,7 +1,16 @@
 import ListGroup from "./components/ListGroup";
 
+import { Chess } from 'chess.js'
+
 function App() {
-  return <div><ListGroup /></div>;
+
+  const chess = new Chess()
+  while(!chess.isGameOver()) {
+  const moves = chess.moves()
+  const move = moves[Math.floor(Math.random() * moves.length)]
+  chess.move(move)
+  }
+  return <p>{chess.pgn()}</p>
 }
 
 export default App;
